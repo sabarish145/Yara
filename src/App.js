9f274749b6236b7ce998bac927aeaacc7f3adf3e
sabarish <sabarish.l.k@macys.com>
2024-10-27 22:53:38 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+ 
+function App() {
+ 
+  return (
+    
+<df-messenger
+  project-id="cb1929840a-1000389672-gc"
+  agent-id="d9baf03d-2d28-4627-8f06-28d14aca4651"
+  language-code="en"
+  max-query-length="-1">
+  <df-messenger-chat-bubble
+   chat-title="Yara6.0">
+  </df-messenger-chat-bubble>
+</df-messenger>
+
+ 
+  );
+}
+ 
+export default App;
+ 
